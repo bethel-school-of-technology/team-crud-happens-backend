388eb5206cb38710c21e418339fd5f0c424bbaf9
ValdemirGoncalves <infojr.83@gmail.com>
2021-06-14 12:26:24 -0400

    creating a create Post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,14 @@
+import PostMessage from "../models/postMessage.js";
+export const getPosts = async(req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
 
-export const getPosts = (req,res) => {
-    res.send('Its working!');
-}; 
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+export const createPost = async(req, res) => {
+    res.send('Post Creation');
+}
