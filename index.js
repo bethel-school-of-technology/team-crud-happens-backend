f67172fba225f042cd3a3f47b5d93bc8087f5c5c
ValdemirGoncalves <infojr.83@gmail.com>
2021-06-14 20:45:04 -0400

    mongodb fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-app.use('/posts', postRoutes);
+
 
 app.use(express.json({ limit: '10mb', extended: true }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
@@ -13,11 +13,11 @@ app.use(cors());
 
 
 
-const CONNECTION_URL = 'mongodb+srv://crudhappens:crudhappens123@cluster0.fhbmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://crud:crud123456@cluster0.kwhxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost: ${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
