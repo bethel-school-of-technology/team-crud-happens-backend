0c6fc1630c6b37acdce46d299aa96c3451fe89a0
John <johnnyrrodgers@gmail.com>
2021-06-29 17:32:41 -0500

    junior

--- routes/posts.js
+++ routes/posts.js
@@ -29,29 +29,29 @@ const uploader = multer({
         fileSize: 5 * 1024 * 1024, // limiting files size to 5 MB
     },
 });
-app.post('/', uploader.single('image'), async(req, res, next) => {
-try {
-if (!req.file) {
-res.status(400).send('Error, could not upload file');
-return;
-}
-const blob = bucket.file(req.file.originalname);
-const blobWriter = blob.createWriteStream({
-metadata: {
-contentType: req.file.mimetype,
-},
-});
-blobWriter.on('error', (err) => next(err));
-blobWriter.on('finish', () => {
-const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name }/o/${encodeURI(blob.name)}?alt=media`;
+app.post('/', uploader.single('image'), async (req, res, next) => {
+    try {
+        if (!req.file) {
+            res.status(400).send('Error, could not upload file');
+            return;
+        }
+        const blob = bucket.file(req.file.originalname);
+        const blobWriter = blob.createWriteStream({
+            metadata: {
+                contentType: req.file.mimetype,
+            },
+        });
+        blobWriter.on('error', (err) => next(err));
+        blobWriter.on('finish', () => {
+            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURI(blob.name)}?alt=media`;
 
-res.status(200).send({ fileName: req.file.originalname, fileLocation: publicUrl });
-});
-blobWriter.end(req.file.buffer);
-} catch (error) {
-res.status(400).send(`Error, could not upload file: ${error}`);
-return;
-}
+            res.status(200).send({ fileName: req.file.originalname, fileLocation: publicUrl });
+        });
+        blobWriter.end(req.file.buffer);
+    } catch (error) {
+        res.status(400).send(`Error, could not upload file: ${error}`);
+        return;
+    }
 });
 
 export default router;
